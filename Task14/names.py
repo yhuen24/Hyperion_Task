85b1e2f2a4c5599381386b70e6c444fb2719a017
Spicy Ramen <56414768+yhuen24@users.noreply.github.com>
2022-11-05 20:31:16 +0000

    commit

---
+++ Task14/names.py
@@ -0,0 +1,10 @@
+counter = 0
+
+# Creates an infinite loop that only breaks when user inputs "Stop"
+while True:
+    name = input("Enter student name: ")
+    # makes name lower case so that capitalization don't matter
+    if name.lower() == "stop":
+        print(counter)
+        break
+    counter += 1  # add 1 to counter for every name inputted
