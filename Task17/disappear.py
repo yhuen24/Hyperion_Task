12876d2fc1011ac055a172b470b180869daf4941
Spicy Ramen <56414768+yhuen24@users.noreply.github.com>
2022-11-09 15:03:47 +0000

    Task 17 reDone

--- Task17/disappear.py
+++ Task17/disappear.py
@@ -1,4 +1,7 @@
 sentence = input("Enter a sentence: ")
 remove_character = input("Which character would you like to disappear: ")
-new_sentence = sentence.replace(remove_character, "")
+new_sentence = ""
+for ch in sentence:
+    if ch not in list(remove_character):
+        new_sentence += ch
 print(new_sentence)
