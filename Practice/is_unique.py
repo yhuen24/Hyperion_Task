3058ade453ea7d69d30fcef4bfa5eea0edb41ea7
Spicyhuen <hyuen.yutico@gmail.com>
2022-11-11 18:46:05 +0000

    is_unique optimized solution

--- Practice/is_unique.py
+++ Practice/is_unique.py
@@ -1,23 +1,13 @@
 # Implement an algorithm to determine if a string has all unique characters.
 # What if you cannot use additional data structures?
 def check_is_unique(my_string):
-    flag = 0
-    hashmap = {}
+    ch_set = set()
     for ch in my_string:
-        if ch not in hashmap:
-            hashmap[ch] = 1
+        if ch not in ch_set:
+            ch_set.add(ch)
         else:
-            hashmap[ch] += 1
-            flag = 1
-
-    if flag == 1:
-        for key in hashmap:
-            if hashmap[key] > 1:
-                print(f"There is {hashmap[key]} number of {key} in your string")
-        return "Your string has duplicate characters"
-    else:
-        return "Your string has all unique characters"
-
+            return "Your string has duplicate characters"
+    return "Your string has all unique characters"
 
 input_string = input("Enter a string: ")
 print(check_is_unique(input_string))
