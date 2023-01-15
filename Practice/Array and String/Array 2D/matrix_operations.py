4ce9a9d564e3058d9e3559db6044efc7411ba4cc
Spicy Ramen <56414768+yhuen24@users.noreply.github.com>
2023-01-15 20:19:00 +0000

    matrix operations

--- Practice/Array and String/Array 2D/matrix_operations.py
+++ Practice/Array and String/Array 2D/matrix_operations.py
@@ -9,11 +9,32 @@ def take_input():
     return user_input
 
 
+def matrix_multiply(matrix1, matrix2):
+    if len(matrix1[0]) != len(matrix2):
+        return None
+    new_row = len(matrix1)
+    new_col = len(matrix2[0])
+    result = [[0 for _ in range(new_col)] for _ in range(new_row)]
+
+    for i in range(len(matrix1)):
+        for j in range(len(matrix2[i])):
+            for k in range(len(matrix1[0])):
+                result[i][j] += matrix1[i][k] * matrix2[k][j]
+    return result
+
+
+def scalar_multiply(num, matrix):
+    new_row = len(matrix)
+    new_col = len(matrix[0])
+    result = [[0 for _ in range(new_col)] for _ in range(new_row)]
+    for i in range(new_row):
+        for j in range(new_col):
+            result[i][j] = matrix[i][j] * num
+    return result
+
+
 matrix1 = take_input()
-matrix2 = take_input()
-
-for row in matrix1:
-    print(row)
-
-for row in matrix2:
+num = int(input("Enter Scalar value: "))
+result = scalar_multiply(num, matrix1)
+for row in result:
     print(row)
