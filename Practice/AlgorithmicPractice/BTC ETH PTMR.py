100606f4591aabc4e360344466147cb7263840fd
Spicy Ramen <56414768+yhuen24@users.noreply.github.com>
2023-07-17 15:23:50 +0100

    Pairs Trading Mean Reversion for BTC-USD and ETH-USD pair

--- Practice/AlgorithmicPractice/BTC ETH PTMR.py
+++ Practice/AlgorithmicPractice/BTC ETH PTMR.py
@@ -1,7 +1,6 @@
 # Mean Reversion Strategy using Pairs Trading of BTC and ETH
 import pandas as pd
 import yfinance as yf
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
