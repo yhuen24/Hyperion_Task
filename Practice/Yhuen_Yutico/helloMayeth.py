09d9e697e16d24d47bcb469f75bc208f54692802
Spicyhuen <hyuen.yutico@gmail.com>
2023-07-17 13:31:02 +0100

    smalles

---
+++ Practice/Yhuen_Yutico/helloMayeth.py
@@ -0,0 +1,7 @@
+list1 = ["mayeth", "yhuen", "barjugan", 45, "mutmut", "boracay", "ihaw pusit", "love", 45]
+clean_list1 = set(list1)
+print(list1)
+print(clean_list1)
+
+for word in clean_list1:
+    print(word)
