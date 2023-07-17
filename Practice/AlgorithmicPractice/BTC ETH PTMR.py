f088036ec611e4eb22fa1f0ac1ad2d79c6ad2a39
Spicyhuen <hyuen.yutico@gmail.com>
2023-07-17 15:28:43 +0100

    refactor a bit

--- Practice/AlgorithmicPractice/BTC ETH PTMR.py
+++ Practice/AlgorithmicPractice/BTC ETH PTMR.py
@@ -8,10 +8,12 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 frames = {}
 
+
 def create_frame(ticker_input, start_date, end_date):
     ticker = yf.Ticker(ticker_input)
-    data = ticker.history(start = start_date, end = end_date)
+    data = ticker.history(start=start_date, end=end_date)
     return pd.DataFrame(data)
+
 
 def scatter_plot(asset1, asset2):
     sns.jointplot(x=asset1, y=asset2, kind='scatter', color='blue')
@@ -19,6 +21,7 @@ def scatter_plot(asset1, asset2):
     plt.ylabel("ETH Close Price")
     plt.title("Jointplot of BTC and ETH Close Prices")
     plt.show()
+
 
 frames["btc"] = create_frame("BTC-USD", "2018-01-01", "2023-06-30")
 frames["eth"] = create_frame("ETH-USD", "2018-01-01", "2023-06-30")
@@ -28,10 +31,12 @@ for frame in frames:
     del frames[frame]["Dividends"]
     del frames[frame]["Stock Splits"]
 
+
 def calculate_correlation(asset1, asset2, column):
     btc_close = frames[asset1][column]
     eth_close = frames[asset2][column]
     return eth_close.corr(btc_close)
+
 
 def entry_exit_conditions(asset1, asset2, entry_threshold, exit_threshold, column):
     asset1_close = frames[asset1][column]
@@ -48,14 +53,18 @@ def entry_exit_conditions(asset1, asset2, entry_threshold, exit_threshold, colum
     signals = pd.Series(0, index=asset1_close.index)
 
     # Long ass1 and short ass2 when ass1 price is below the lower Bollinger Band and ass2 price is above the upper Bollinger Band
-    signals[(asset1_close < asset1_sma - entry_threshold * asset1_std) & (asset2_close > asset2_sma + entry_threshold * asset2_std)] = 1
+    signals[(asset1_close < asset1_sma - entry_threshold * asset1_std) & (
+                asset2_close > asset2_sma + entry_threshold * asset2_std)] = 1
 
     # Short as11 and long ass2 when ass1 price is above the upper Bollinger Band and ass2 price is below the lower Bollinger Band
-    signals[(asset1_close > asset1_sma + entry_threshold * asset1_std) & (asset2_close < asset2_sma - entry_threshold * asset2_std)] = -1
+    signals[(asset1_close > asset1_sma + entry_threshold * asset1_std) & (
+                asset2_close < asset2_sma - entry_threshold * asset2_std)] = -1
 
     # Exit signals when BTC and ETH prices move back within the exit_threshold range of their respective means
-    exit_condition = (asset1_close >= asset1_sma - exit_threshold * asset1_std) & (asset1_close <= asset1_sma + exit_threshold * asset1_std) & \
-                     (asset2_close >= asset2_sma - exit_threshold * asset2_std) & (asset2_close <= asset2_sma + exit_threshold * asset2_std)
+    exit_condition = (asset1_close >= asset1_sma - exit_threshold * asset1_std) & (
+                asset1_close <= asset1_sma + exit_threshold * asset1_std) & \
+                     (asset2_close >= asset2_sma - exit_threshold * asset2_std) & (
+                                 asset2_close <= asset2_sma + exit_threshold * asset2_std)
     signals[exit_condition] = 0
 
     return signals
@@ -75,6 +84,7 @@ def calculate_daily_returns(asset1, asset2, signals, column):
 
     return daily_returns
 
+
 def plot_cumulative_returns(cumulative_returns):
     plt.figure(figsize=(10, 6))
     plt.plot(cumulative_returns.index, cumulative_returns, label="Cumulative Returns", color="green")
@@ -84,6 +94,7 @@ def plot_cumulative_returns(cumulative_returns):
     plt.legend()
     plt.grid(True)
     plt.show()
+
 
 def backtest(asset1, asset2, column):
     signals = entry_exit_conditions(asset1, asset2, entry_threshold, exit_threshold, column)
@@ -98,4 +109,3 @@ entry_threshold = 0.6
 exit_threshold = 0.25
 profit_ETHBTC = backtest("btc", "eth", "Close").round(2)
 print(f"BTC to ETH Overall profit: {profit_ETHBTC}%")
-
